2a99a35258d2048a660ad075bf5c5ffebd15a13b
Josefine Lantz <jo.lantz@icloud.com>
2024-12-13 20:11:09 +0100

    comment out not used code

--- main.py
+++ main.py
@@ -6,24 +6,44 @@ from scripts.analyze_scores import group_variants
 from scripts.visualize import plot_box, plot_stacked, plot_box_distribution, plot_hist_distribution, plot_grouped_data, plot_variant_scores
 
 def main(vcf_path, output_dir):
+    ### PARSE ###
     print("Parsing VCF...")
     df = parse_vcf(vcf_path)
-    df.to_csv("df.csv")
 
-    plot_variant_scores(df[["Group", "RankScore"]])
+    ### INSPECT DATA ###
+    print(df.head())
+
+    ### SCATTER PlOTS ### 
+
+    ### (Variants on x-axis, RankScore on y-axis)
+
+    ### (Variants on x-axis, AF score on y-axis)
+
+    ### (Variants on x-axis, PP score on y-axis)
+
+    ### Variants on x-axis, CON score on y-axis)
+
+    ### (Variants on x-axis, VCQF score on y-axis)
+
+    ### (Variants on x-axis, VAF score on y-axis)
+
+    ### (Variants on x-axis, CLIN score on y-axis)
+
+
+    #plot_variant_scores(df[["Group", "RankScore"]])
    # print("Grouping variants...")
-    groups, counts, grouped_data_groups, grouped_data_all = group_variants(df)
-    print("Variant counts:", counts)
+   # groups, counts, grouped_data_groups, grouped_data_all = group_variants(df)
+    #print("Variant counts:", counts)
 
-    print("Generating box plot...")
-    plot_box(groups, output_path=f"{output_dir}/boxplot.png")
+    #print("Generating box plot...")
+    #plot_box(groups, output_path=f"{output_dir}/boxplot.png")
     
    # print("Plotting rank_score distribution....")
     #plot_box_distribution(df, output_path=f"{output_dir}/rank_score_dist_box.png")
     # plot_hist_distribution(df, output_path=f"{output_dir}/rank_score_dist_hist.png")
 
     # print("Plotting grouped_data....")
-    plot_grouped_data(grouped_data_groups, output_path=f"{output_dir}/grouped_data.png")
+    #plot_grouped_data(grouped_data_groups, output_path=f"{output_dir}/grouped_data.png")
     
     # print("Generating stacked plot...")
     # plot_stacked(groups, output_path=f"{output_dir}/stackedplot.png")
