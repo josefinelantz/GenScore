35c5bf5259331561f002a7defd69e3cb92b2f0f2
Josefine Lantz <jo.lantz@icloud.com>
2024-12-30 09:42:25 +0100

    update analyze scores

--- scripts/analyze_scores.py
+++ scripts/analyze_scores.py
@@ -1,9 +1,13 @@
 import pandas as pd 
 
 def analyze_scores(df):
+    """
+    Returns the df with column GROUP
+    """
     # Group variants into benign/pathogenic
     benign_terms = {"Benign", "Likely_benign"}
     pathogenic_terms = {"Pathogenic", "Likely_pathogenic"}
+    
     def classify_group(row):
         if row["CLNSIG"] in benign_terms:
             return "benign"
@@ -11,24 +15,27 @@ def analyze_scores(df):
             return "pathogenic"
         return "control"
 
-    def assign_color(group):
-        return {"benign": "blue", "pathogenic": "red", "control": "yellow"}[group]
-
     df["GROUP"] = df.apply(classify_group, axis=1)
-    df["COLOR"] = df["GROUP"].apply(assign_color)
     return df
 
 def mark_controls(df, control_tsv_path):
+    """
+    Matches controls in VCF
+    Assumes control_tsv_path has column:
+    chr_pos
+    Returns df with column IS_CONTROL
+    """
     controls = pd.read_csv(control_tsv_path, sep="\t")
     controls["chr_pos"] = controls["Chrom"].astype(str) +"_"+ controls["Position"].astype(str)
     controls.set_index("chr_pos", inplace=True)
     positive_controls = list(controls.index)
     df["is_control"] = df["CHROM_POS"].isin(positive_controls)
-    #control_set = set(controls["CHROM_POS"])
-    #df["IS_CONTROL"] = df["CHROM_POS"].isin(control_set)
     return df
 
 def subtract_clin_score(df):
+    """
+    Returns df with column NO_CLIN_RANK_SCORE
+    """
     df["NO_CLIN_RANK_SCORE"] = df["RANK_SCORE"] - df["CLIN"]
     return df
 
