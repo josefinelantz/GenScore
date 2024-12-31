96613c6f544225e2472f955fc34e609edf9e9202
Josefine Lantz <jo.lantz@icloud.com>
2024-12-31 18:54:36 +0100

    update parce_vcf and added basic test

--- scripts/parse_vcf.py
+++ scripts/parse_vcf.py
@@ -1,34 +1,55 @@
-# scripts/parse_vcf.py
-
 from cyvcf2 import VCF
 import pandas as pd
 
+def parse_csq(fields, csq_str):
+    csq = []
+    annotation_list = csq_str.split(",")
+    for annotation in annotation_list:   
+        return csq.append(dict(zip(fields, annotation.split("|"))))
+
 def parse_vcf(file_path):
     """ 
-    Reads VCF and returns df with colums:
+    Reads VCF, extracts and formats variant information 
+    relevant to scoring.  
+    Args: 
+        file_path (str): path to a valid VCF. 
+    
+    Returns (pd.DataFrame): df with colums:
     CHROM_POS_REF_ALT, CHROM_POS, AF, PP, CON, VCQF,
-    LIN, CLIN, CLNSIG, RANK_SCORE
+    LIN, CLIN, CLNSIG, RANK_SCORE, PARSED_CSQ, AAF(aaf), FILTER, VAF(AF), COVERAGE(DP)
     """
-    # load the VCF 
+    # Load VCF into cyvcf2.VCF generator object 
     vcf = VCF(file_path)
-    # declare variable for extracted data
+
+    # Get the headers for VEP annotations
+    csq_fields = vcf.get_header_type("CSQ")["Description"][51:-1].split("|")
+    
     data = []
-    # iterate over variant in the VCF and extract required fields
+    # Iterate the variants loaded and extract required fields
     for variant in vcf:
         chrom = variant.CHROM
         pos = variant.POS
         ref = variant.REF
         alt = variant.ALT[0]
         info = variant.INFO
+        aaf = variant.aaf 
+        filter_value = variant.FILTER
+
+        # extract INFO-keys
+        csq_str = info.get("CSQ", "") 
+        af, pp, con, vcqf, lin, clin = [float(x) for x in info.get("RankResult", "0|0|0|0|0|0").split("|")]   
+        rank_score = float(info.get("RankScore")[2:])    
+        clnsig = info.get("CLNSIG", "no_value") 
+        vaf = info.get("AF", "")
+        coverage = info.get("DP", "")
+
+        # Extract CSQ-keys 
+        csq = parse_csq(csq_fields, csq_str)
+
         # format strings for extracting controls 
         chrom_pos = f"{chrom}_{pos}"
         ref_alt = f"{ref}_{alt}"
-        # separate category scores and convert to numerical values 
-        af, pp, con, vcqf, lin, clin = [float(x) for x in info.get("RankResult", "0|0|0|0|0|0").split("|")]
-        # extract rank score
-        rank_score = float(info.get("RankScore")[2:])
-        # extract CLNSIG, if not exist, return "no_value" to avoid missing values 
-        clnsig = info.get("CLNSIG", "no_value") 
+       
         # append the extracted data to the output list
         data.append({
             "VARIANT": f"{chrom_pos}_{ref_alt}",
@@ -40,7 +61,12 @@ def parse_vcf(file_path):
             "LIN": lin,
             "CLIN": clin,
             "CLNSIG": clnsig,
-            "RANK_SCORE": rank_score
+            "RANK_SCORE": rank_score,
+            "PARSED_CSQ": csq,
+            "AAF(aaf)": aaf,
+            "FILTER": filter_value,
+            "VAF(AF)": vaf,
+            "COVERAGE(DP)": coverage
         })
 
     return pd.DataFrame(data)
--- tests/test_parse_vcf.py
+++ tests/test_parse_vcf.py
@@ -1,7 +1,5 @@
-# tests/test_parse_vcf.py
-
 import pytest
-from scripts.parse_vcf import parse_vcf
+from scripts.parse_vcf import parse_vcf, parse_csq
 import pandas as pd
 
 def test_parse_vcf():
@@ -9,16 +7,18 @@ def test_parse_vcf():
     df = parse_vcf(test_vcf)
     
     assert isinstance(df, pd.DataFrame)
-    assert "CLNSIG" in df.columns
-    assert "RankScore" in df.columns
+    assert "VARIANT" in df.columns
+    assert "CHROM_POS" in df.columns
     assert "VARIANT" in df.columns
     assert  "AF" in df.columns
     assert  "PP" in df.columns
     assert  "CON" in df.columns
     assert  "VCQF" in df.columns
-    assert  "VAF" in df.columns
+    assert  "LIN" in df.columns
     assert  "CLIN" in df.columns
     assert  "CLNSIG" in df.columns
-    assert  "RankScore" in df.columns
-    assert  "Group" in df.columns
-   
+    assert  "RANK_SCORE" in df.columns
+    assert  "PARSED_CSQ" in df.columns
+    assert  "AAF(aaf)" in df.columns
+    assert  "VAF(AF)" in df.columns
+    assert  "COVERAGE(DP)" in df.columns
